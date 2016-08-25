de9b700a36bab87f5bf2b76bf1fe4d0b3e9dc2a0
guanhui1998 <lguanhui14@gmail.com>
2016-08-25 14:57:00 +0800

    added tga from discarded framework

---
+++ Base/Source/GameObject.h
@@ -0,0 +1,33 @@
+#ifndef GAME_OBJECT_H
+#define GAME_OBJECT_H
+
+#include "Vector3.h"
+#include <string>
+
+using std::string;
+
+struct GameObject
+{
+	enum GAMEOBJECT_TYPE
+	{
+		GO_NONE = 0,
+		GO_BALL,
+
+		GO_TOTAL, //must be last
+	};
+	GAMEOBJECT_TYPE type;
+	Vector3 pos;
+	Vector3 vel;
+	Vector3 scale;
+    Vector3 normal;
+	bool active;
+	float mass;
+    string name;
+
+	GameObject(GAMEOBJECT_TYPE typeValue = GO_BALL);
+    void Init();
+    void exit();
+	~GameObject();
+};
+
+#endif
