24673eb7ab7e8070051667472750929f204295f3
guanhui1998 <lguanhui14@gmail.com>
2016-08-29 14:31:26 +0800

    more work on sprite manager class

--- Base/Source/SpriteManager.cpp
+++ Base/Source/SpriteManager.cpp
@@ -40,29 +40,53 @@ void SpriteManager::update(double dt)
 }
 
 void SpriteManager::RenderSprite(Mesh *mesh, bool enableLight, float size, float x, float y, bool rotate, bool flip)
-{
-    for (std::vector<SpriteAnimation*>::iterator it = SpriteAnimation_ListRender.begin(); it != SpriteAnimation_ListRender.end(); ++it)
-    {
-        SpriteAnimation* go = (SpriteAnimation*)*it;
-        
-    }
-            
+{    
+
+    Render2DMesh(mesh, enableLight, size, x, y, rotate, flip);
 }
-SpriteAnimation* SpriteManager::NewSpriteAnimation(Mesh *mesh, Vector3 pos, int row, int col,
-    int start, int end, float speed, int no_of_repeat)
+void SpriteManager::NewSpriteAnimation(Mesh *mesh, Vector3 pos, Vector3 scale, int row, int col,
+    int start, int end, float speed, int no_of_repeat, bool flip)
 {
     SpriteAnimation* go = new SpriteAnimation(mesh->name, row, col);
     go->m_anim = new Animation();
     go->m_anim->Set(start, end, no_of_repeat, speed, false);
+    sprite* N_sprite = new sprite(go, pos, scale, flip, mesh);
+    spritelist.push_back(N_sprite);
     //SpriteAnimation_List.push_back(go);
-    return go;
+    //return go;
 }
 
-void SpriteManager::PlaySprite(SpriteAnimation* Sprite_Animation)
+//void SpriteManager::PlaySprite(SpriteAnimation* Sprite_Animation)
+//{
+//    //SpriteAnimation sp = *Sprite_Animation;
+//    SpriteAnimation* go = new SpriteAnimation(*Sprite_Animation);
+//    go->m_anim->animActive = true;
+//    
+//    SpriteAnimation_ListRender.push_back(go);
+//}
+void SpriteManager::spriteRender()
 {
-    //SpriteAnimation sp = *Sprite_Animation;
-    SpriteAnimation* go = new SpriteAnimation(*Sprite_Animation);
-    go->m_anim->animActive = true;
-    SpriteAnimation_ListRender.push_back(go);
+    for (std::vector<sprite*>::iterator it = spritelist.begin(); it != spritelist.end(); ++it)
+    {
+        sprite* go = (sprite*)*it;
+        Render2DMesh(go->mesh, false, go->scale.x, go->scale.y, go->pos.x, go->pos.x, go->flip, false);
+    }
 }
 
+//sprite
+sprite::sprite(SpriteAnimation* spriteanim,
+    Vector3 pos,
+    Vector3 scale,
+    bool flip, Mesh *mesh) :
+    spriteanim(spriteanim),
+    pos(pos),
+    scale(scale),
+    flip(flip),
+    mesh(mesh)
+{
+
+}
+sprite::~sprite()
+{
+    
+}
